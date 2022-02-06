89bcaca140eea226fa26828a642a6fd990b36e6d
sheshum <vladimir.sesum@gmail.com>
2022-02-06 11:18:17 +0100

    inital commit for wasm rust

---
+++ gol-simulator-wasm-rs/src/lib.rs
@@ -0,0 +1,29 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen(module = "/www/assets/js/screen.js")]
+extern {
+    fn add_cell();
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, gol-simulator-wasm-rs!");
+}
+
+#[wasm_bindgen]
+pub fn tick() {
+    add_cell();
+}
